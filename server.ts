aae200140b86fea77a9dc51bdc4d590171091af2
Chibbit99 <chibbit99@gmail.com>
2026-05-28 10:59:48 +1000

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,10 @@
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
+Deno.serve({ port: 8000 }, (req) => {
+  if (req.method === "OPTIONS") return new Response(null, { headers: corsHeaders });
+  return new Response(JSON.stringify({ msg: "CORS-enabled" }), { headers: corsHeaders });
+});
